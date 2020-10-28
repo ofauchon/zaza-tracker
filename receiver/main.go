95d6b37bf67b8877fd26afc9bb38461e3fbceb22
Olivier Fauchon <ofauchon2204@gmail.com>
2020-10-28 01:14:11 +0100

    Fixed external interrupts for Button and RFM95 DIO0, added channel for packet reception

--- receiver/main.go
+++ receiver/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"device/stm32"
-	"fmt"
 	"machine"
 	"runtime/interrupt"
 	"runtime/volatile"
@@ -43,6 +42,9 @@ var (
 	keypressed           bool
 )
 
+// Channel for packet RX
+var rxPktChan chan []byte
+
 // processCmd parses commands and execute actions
 func processCmd(cmd string) {
 	ss := strings.Split(cmd, " ")
@@ -59,7 +61,7 @@ func processCmd(cmd string) {
 		loraRadio.Reset()
 		println("Reset done !")
 
-	// Send 1 packet every 10 Seconds
+	// Send Lora packets
 	case "loratx":
 		if len(ss) == 2 {
 			keypressed = false
@@ -78,26 +80,29 @@ func processCmd(cmd string) {
 			}()
 
 		}
-	// Listen for Lora packets for 20 Seconds
+
+	// Listen for Lora packets
 	case "lorarx":
 		keypressed = false
 		go func() {
-			//Continuous mode leaves radio always on
+			println("lorarx: Start RXContinuous")
 			loraRadio.ReceiveContinuous()
 
 			for !keypressed {
+				println("RX packet: Waiting for new packet")
+				packet := <-rxPktChan
+				println("RX packet: New RX", string(packet))
 
-				packetSize := loraRadio.ParsePacket(0)
-				if packetSize > 0 {
-					//println("Got packet, RSSI=", loraRadio.LastPacketRSSI())
-					size := loraRadio.ReadPacket(packet[:])
-					println("RX: ", string(packet[:size]), " packetsize", packetSize)
-				}
-
-				time.Sleep(500 * time.Millisecond)
+				/*
+					packetSize := loraRadio.ParsePacket(0)
+					if packetSize > 0 {
+						//println("Got packet, RSSI=", loraRadio.LastPacketRSSI())
+						size := loraRadio.ReadPacket(packet[:])
+						println("RX: ", string(packet[:size]), " packetsize", packetSize)
+					}
+				*/
 			}
-
-			println("LoraRX: Stopped by user")
+			println("lorarx: Stop RXContinuous")
 		}()
 
 	case "get":
@@ -197,70 +202,99 @@ func serial(serial *machine.UART) string {
 
 }
 
-func Handler_EXTI4_15(inter interrupt.Interrupt) {
-	println("IRQ: ", stm32.EXTI.PR.Get())
-	loraRadio.ParsePacket(0)
+// Interrupt handler from RFM95_DIO0 (RxDone Event) on PB13
+func gpios_int(inter interrupt.Interrupt) {
+	irqStatus := stm32.EXTI.PR.Get()
+
+	// Crear flag
+	stm32.EXTI.PR.Set(irqStatus)
+
+	if (irqStatus & 0x2000) > 0 { // PC13 : DIO
+
+		packetSize := loraRadio.ParsePacket(0)
+		println("pgios_int: packetSize:", packetSize)
+		if packetSize > 0 {
+			size := loraRadio.ReadPacket(packet[:])
+			rxPktChan <- packet[:size]
+		}
+
+	}
+	if (irqStatus & 0x4000) > 0 { // PB14 : Button
+		println("Button: ", machine.BUTTON.Get())
+	}
+
 }
 
-//----------------------------------------------------------------------------------------------//
+// hw_init() is responsible for all hardware init
+// Refs: https://stackoverflow.com/questions/63746239/enable-external-interrupts-arm-cortex-m0-stm32g070
+func hw_init() {
 
-// main is where the program begins :-)
-func main() {
+	// SYSCFGEN is NEEDED FOR IRQ HANDLERS (button + Dio) .. Do not remove
+	stm32.RCC.APB2ENR.SetBits(stm32.RCC_APB2ENR_SYSCFGEN)
 
-	// Led
-	machine.LED_RED.Configure(machine.PinConfig{Mode: machine.PinOutput})
-
-	//Button
+	// BUTTON PB14
 	machine.BUTTON.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+	stm32.SYSCFG_COMP.EXTICR4.ReplaceBits(0b001, 0xf, stm32.SYSCFG_EXTICR4_EXTI14_Pos) // Enable PORTB On line 14
+	stm32.EXTI.RTSR.SetBits(stm32.EXTI_RTSR_RT14)                                      // Detect Rising Edge of EXTI14 Line
+	stm32.EXTI.FTSR.SetBits(stm32.EXTI_FTSR_FT14)                                      // Detect Falling Edge of EXTI14 Line
+	stm32.EXTI.IMR.SetBits(stm32.EXTI_IMR_IM14)                                        // Enable EXTI14 line
 
+	// GPIOS: Leds
+	machine.LED_RED.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.LED_GREEN.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.LED_BLUE.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	// UART0 (Console)
 	uartConsole = &machine.UART0
 	uartConsole.Configure(machine.UARTConfig{TX: machine.UART_TX_PIN, RX: machine.UART_TX_PIN, BaudRate: 9600})
-	// SPI and lx1276
+	go serial(uartConsole)
+	// Spi bus configuration
 	machine.SPI0.Configure(machine.SPIConfig{})
+	// Lora sx1276/rfm95 configuration
 	csPin := machine.PA15
 	csPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	rstPin := machine.PB0
 	rstPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	loraRadio = sx127x.New(machine.SPI0, csPin, rstPin)
-	var err = loraRadio.Configure(loraConfig)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// RFM95 DIO0 Interrupt
-	machine.RFM95_DIO0_PIN.Configure(machine.PinConfig{Mode: machine.PinInputFloating})
-	//stm32.RCC.APB2ENR.SetBits(stm32.RCC_APB2ENR_AFIOEN)                                // Enable AFIO
-	stm32.SYSCFG_COMP.EXTICR4.ReplaceBits(0b001, 0xf, stm32.SYSCFG_EXTICR4_EXTI13_Pos) // Enable PORTB EXTI only
-	stm32.EXTI.RTSR.SetBits(stm32.EXTI_RTSR_RT13)                                      // Detect Rising Edge of EXTI0 Line
-	stm32.EXTI.IMR.SetBits(stm32.EXTI_IMR_IM13)                                        // Enable EXTI0 line
-
-	intr := interrupt.New(stm32.IRQ_EXTI4_15, Handler_EXTI4_15)
-	intr.SetPriority(0xc0)
+	loraRadio.Configure(loraConfig)
+	// Configure PB13 (connected to DIO0.rfm95)
+	machine.RFM95_DIO0_PIN.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	stm32.SYSCFG_COMP.EXTICR4.ReplaceBits(0b010, 0xf, stm32.SYSCFG_EXTICR4_EXTI13_Pos) // Enable PORTC On line 13
+	stm32.EXTI.RTSR.SetBits(stm32.EXTI_RTSR_RT13)                                      // Detect Rising Edge of EXTI13 Line
+	stm32.EXTI.FTSR.SetBits(stm32.EXTI_FTSR_FT13)                                      // Detect Falling Edge of EXTI13 Line
+	stm32.EXTI.IMR.SetBits(stm32.EXTI_IMR_IM13)                                        // Enable EXTI13 line
+	// Enable interrupts
+	intr := interrupt.New(stm32.IRQ_EXTI4_15, gpios_int)
+	intr.SetPriority(0x0)
 	intr.Enable()
+}
 
-	go serial(uartConsole)
+//----------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------//
+// main is where the program begins :-)
+//----------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------//
+func main() {
 
-	//-----------------------------------------------------
-	// Start
-	println("*************")
-	println("Zaza Receiver")
-	println("*************")
+	// Packets will be sent to rxPktChan Channel
+	rxPktChan = make(chan []byte)
 
-	println("Press help for commands.")
+	hw_init()
+
+	println("*** Zaza Receiver ***")
+	println("Press ? for commands.")
 
 	// 3 Green blinks at start
-	for i := 0; i < 3; i++ {
-		machine.LED_GREEN.Set(true)
+	for i := 0; i < 6; i++ {
+		machine.LED_GREEN.Set((i % 2) == 0)
 		time.Sleep(250 * time.Millisecond)
-		machine.LED_GREEN.Set(false)
-		time.Sleep(1 * time.Second)
 	}
 
+	// Force RX
 	processCmd("lorarx")
 
+	// Wait forever
 	for {
+		//		println("Button: ", machine.BUTTON.Get())
 		time.Sleep(1 * time.Second)
 	}
 
