907fcbd425d2c4d6fd499cf4963c58b9988b766c
Olivier Fauchon <ofauchon2204@gmail.com>
2021-09-21 16:53:49 +0200

    Add binary representation function

--- libs/tools.go
+++ libs/tools.go
@@ -28,6 +28,29 @@ func BytesToHexString(data []byte) string {
 	return s
 }
 
+//ByteToString returns string with binary representation of val
+func IntToBinString(val int, bitsize int) string {
+	s := ""
+	for j := (bitsize - 1); j >= 0; j-- {
+		if val&(1<<j) != 0 {
+			s += "1"
+		} else {
+			s += "0"
+		}
+	}
+	return s
+}
+
+//BytesToBinString returns binary string representation of byte array
+func BytesToBinString(data []byte) string {
+	s := ""
+	for i := 0; i < len(data); i++ {
+		s += IntToBinString(int(data[i]), 8)
+		s += " "
+	}
+	return s
+}
+
 // revert inverts de order of a given byte slice
 func Revert(s []byte) []byte {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
